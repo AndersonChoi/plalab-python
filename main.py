bbc567685629ac8c8ba637464131b743e9573195
최원영(WonYoung Choi)/Data Infrastructure팀/SKP <circlezero@sk.com>
2019-11-21 11:24:57 +0900

    add delete update add methods

--- main.py
+++ main.py
@@ -8,43 +8,87 @@ todo_list = '''[
   {
     "id": 1,
     "title": "first todo",
-    "description": "pick up milk",
-    "complete": true,
-    "canceled": true,
-    "date": 1389878466730
+    "description": "pick up milk"
   },
   {
     "id": 2,
     "title": "second todo",
-    "description": "learn backbone",
-    "complete": false,
-    "canceled": false,
-    "date": 1389988926901 
+    "description": "learn backbone"
   },
   {
     "id": 3,
     "title": "third todo",
-    "description": "go for a run",
-    "complete": false,
-    "canceled": false,
-    "date": 13899924942401  
+    "description": "go for a run"
   }
 ]'''
 
 json_todo = json.loads(todo_list)
 
-@app.route("/")
-def hello():
+@app.route("/", methods=['GET'])
+def get():
     return jsonify(json_todo)
 
 
 @app.route("/add", methods=['POST'])
 def add():
-  request_json = request.get_json()
+  request_json = json.loads(json.dumps(request.get_json()))
   print(request_json)
+
+  # 중복되지 않는 가장 큰값의 id보다 +1한 값을 추가
+  new_key = {'id':get_latest_id()}
+  request_json.update(new_key)
+
+  # id가 추가된 item을 to-do list에 append
   json_todo.append(request_json)
   print(json_todo)
   return str(json_todo)
 
+
+@app.route("/update", methods=['POST'])
+def update():
+  request_json = request.get_json()
+  print(request_json)
+
+  # element id를 찾아서 삭제
+  delete_element_by_id(request_json['id'])
+
+  # id를 가진 element를 기존 list에 추가
+  json_todo.append(request_json)
+  print(json_todo)
+  return str(json_todo)
+
+
+@app.route("/delete", methods=['DELETE'])
+def delete():
+  request_json = request.get_json()
+  print(request_json)
+
+  # element id를 찾아서 삭제
+  delete_element_by_id(request_json['id'])
+
+  return str(json_todo)
+
+# 중복되지 않는 id를 가지기 위해 신규 추가한 to-do item은 
+# latest id에 1을 추가한 값을 가지도록 한다.
+def get_latest_id():
+  max_id = -1
+  for todo_item in json_todo:
+    if max_id < todo_item['id']:
+      max_id = todo_item['id']
+  return max_id + 1
+
+# element id를 찾아서 삭제
+def delete_element_by_id(id):
+  for i in range(len(json_todo)):
+      if json_todo[i]["id"] == id:
+          json_todo.pop(i)
+          break
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
