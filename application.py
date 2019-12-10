b1c1dd554b3cd24bf4756c2c0f4ef452cb8ac990
최원영(WonYoung Choi)/Data Infrastructure팀/SKP <circlezero@sk.com>
2019-12-10 20:54:21 +0900

    description은 사용하지 않도록 삭제

--- application.py
+++ application.py
@@ -8,18 +8,15 @@ CORS(application)
 todo_list = '''[
   {
     "id": 1,
-    "title": "first todo",
-    "description": "pick up milk"
+    "title": "first todo"
   },
   {
     "id": 2,
-    "title": "second todo",
-    "description": "learn backbone"
+    "title": "second todo"
   },
   {
     "id": 3,
-    "title": "third todo",
-    "description": "go for a run"
+    "title": "third todo"
   }
 ]'''
 
